362cf5a13ce7c9f4d48975073dbda6263b67653c
Roshan Kumar Sharma <roshan@conscent.ai>
2024-06-17 12:27:39 +0530

    getTopStories api add comments

--- composeApp/src/commonMain/kotlin/com/exmaple/newsapp/data/remote/NewsApiClient.kt
+++ composeApp/src/commonMain/kotlin/com/exmaple/newsapp/data/remote/NewsApiClient.kt
@@ -43,6 +43,7 @@ object NewsApiClient {
 
     suspend fun getTopStories(section: String = "science"): NewsData {
 //        val url = "$BASE_URL/top-headlines?country=in&apiKey=9c4ae54511e24ae4b188141138e91f23"
+//        return client.get(url).body()
         return newsData
     }
 }
